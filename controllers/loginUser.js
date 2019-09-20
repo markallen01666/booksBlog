05360622022dc8f56f934cafee3deacd2913fa52
Mark Allen <markallen.webdeveloper@gmail.com>
2019-09-20 12:32:08 +0100

    Added login info validation to 'Login' view

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -1,28 +1,37 @@
 // authenticate user - controller
 
-const bcrypt = require('bcrypt')
-const User = require('../models/User')
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
 module.exports = (req, res) => {
-    const {username, password} = req.body
+  const { username, password } = req.body;
 
-    User.findOne({username:username}, (error, user) => {
-        if (error) {
-            const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
-            req.flash('validationErrors',validationErrors)
-            req.flash('data', req.body)
-            return res.redirect('/auth/login')
-        } else if (user) {     // user exists
-            bcrypt.compare(password, user.password, (error, same) => {
-                if (same) {     // password match
-                    req.session.userId = user._id
-                    loggedInUsername = username
-                    res.redirect('/')
-                }
-                else {          // passwords don't match
-                    res.redirect('/auth/login')
-                }
-            })
-        }
-    })
+  if (password != null && password != "") {
+    User.findOne({ username: username }, (error, user) => {
+      if (user) {
+        // user exists
+        bcrypt.compare(password, user.password, (error, same) => {
+          if (same) {
+            // password match
+            req.session.userId = user._id;
+            loggedInUsername = username;
+            res.redirect("/");
+          } else {
+            errorFunction(req, res)
+          }
+        });
+      } else {
+        errorFunction(req, res)
+      }
+    });
+  } else {
+    errorFunction(req, res)
+  }
+};
+
+const errorFunction = function(req, res) {
+    const validationErrors = ["Invalid user name or password"];
+    req.flash("validationErrors", validationErrors);
+    req.flash("data", req.body);
+    res.redirect("/auth/login");
 }
