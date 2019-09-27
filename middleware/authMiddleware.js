d970699895f9b338a931781da05484d8183c6fab
Mark Allen <markallen.webdeveloper@gmail.com>
2019-09-27 10:02:06 +0100

    Added 'required' property to fields on new user registration, user login, and new post views

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,7 +4,6 @@ const User = require('../models/User')
 
 module.exports = (req, res, next) => {
     User.findById(req.session.userId, (error, user) => {
-        if (user) console.log(user.username)
         if (error || !user) {
             return res.redirect('/')
         }
